e5165f836379c078e94b7938a3b8c419915541e4
Michał <michal.mazanski@student.wat.edu.pl>
2021-04-20 21:28:36 +0200

    Create client.cs
    Zadanie 1 technologia klient-serwer z 1 serwerem i klientem

---
+++ Zadanie 1/client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			byte[] bytes = new byte[1024];
+			try {
+
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp );
+				try {
+					sender.Connect(remoteEP);  
+					Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+                    string wiadomosc = Console.ReadLine();
+                    byte[] msg = Encoding.Unicode.GetBytes(wiadomosc);
+					sender.Send(msg);
+					Console.WriteLine($"Sent: {Encoding.Unicode.GetString(msg)}");
+					sender.Receive(bytes);
+					Console.WriteLine($"Received: {Encoding.Unicode.GetString(bytes)}");
+					sender.Shutdown(SocketShutdown.Both);  
+					sender.Close();
+				} catch (Exception e) {
+					Console.WriteLine($"Unexpected exception : {e.ToString()}");
+				}
+			} catch (Exception e) {
+				Console.WriteLine( e.ToString());
+			}
+        }
+    }
+}
