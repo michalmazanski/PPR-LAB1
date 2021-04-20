527e309afe304e26efddfc04c82a441b17657a76
Michał <michal.mazanski@student.wat.edu.pl>
2021-04-20 22:01:39 +0200

    Create client1.cs
    Zadanie 3 - gniazda UDP

---
+++ Zadanie 3/client1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress broadcast = IPAddress.Parse("192.168.1.255");
+
+        byte[] sendbuf = Encoding.ASCII.GetBytes(args[0]);
+        IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+        s.SendTo(sendbuf, ep);
+
+        Console.WriteLine("Message sent to the broadcast address");
+    }
+}
